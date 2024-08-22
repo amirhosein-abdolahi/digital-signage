f9ee95dfbc5dbcd80e8dcb90fc236e1c81276f05
Amir hosein Abdolahi <amh.ab.1381@gmail.com>
2024-08-22 22:51:07 +0330

    Edit tailwind config : Add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,8 +8,31 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        "sh": ["shabnam", "sans-serif"],
+        sh: ["shabnam", "sans-serif"],
         "sh-bold": ["shabnam-bold", "sans-serif"],
+      },
+      colors: {
+        primary: {
+          1: "#3769CC",
+          2: "#2B529F",
+          3: "#1D3769",
+        },
+        secondary: {
+          1: "#7EBBFF",
+          2: "#2488F7",
+          3: "#1B65B8",
+        },
+        natural: {
+          100: "#E3E3E3",
+          300: "#929DAE",
+          500: "#526787",
+          700: "#1D2E4F",
+          900: "#0C182E",
+        },
+      },
+      backgroundImage: {
+        gradient:
+          "linear-gradient(180deg, rgba(217, 217, 217, 0) 0%, rgba(42, 69, 139, 0.7) 68.97%, #1D325B 97.4%)",
       },
     },
   },
